1149c8bfbc2e1996f83082e7b6aee76fae35a2cf
Burke Libbey <burke@burkelibbey.org>
2011-02-26 00:17:50 -0600

    more JS tweaks

--- lib/handlebar_wax.rb
+++ lib/handlebar_wax.rb
@@ -28,15 +28,15 @@ module HandlebarWax
   
   def self.generate_templates
     templates = {}
-    HandlebarWax.template_files.each do |template_file|
+    template_files.each do |template_file|
       name = template_file.scan(/app\/views\/(.*)\.[^\.]+\.hbs$/).flatten.first
       templates[name] = File.read(template_file)
     end
  
-    script = File.read(File.join(File.dirname(__FILE__), 'templates', 'handlebars_templates.js.hbs'))
+    script = File.read(File.join(File.dirname(__FILE__), 'lib', 'templates', 'handlebars_templates.js.hbs'))
     script.sub!("{{templates}}", Yajl::Encoder.encode(templates)) # How ironic is this?
  
-    File.open(HandlebarWax.output_path, 'w') { |f| f.puts script }
+    File.open(output_path, 'w') { |f| f.puts script }
   end
 
 end
